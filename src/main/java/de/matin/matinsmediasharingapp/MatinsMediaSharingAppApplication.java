eaf9047800943a6e404b02018ec1309963e345cb
Matin <matin1@uni-bremen.de>
2026-05-08 22:39:14 +0200

    chore: initial project setup

---
+++ src/main/java/de/matin/matinsmediasharingapp/MatinsMediaSharingAppApplication.java
@@ -0,0 +1,13 @@
+package de.matin.matinsmediasharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatinsMediaSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatinsMediaSharingAppApplication.class, args);
+    }
+
+}
