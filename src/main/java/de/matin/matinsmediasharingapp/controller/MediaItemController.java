072cff1c9b6f939fc3053acfa15dec7a55ca0364
Matin <matin1@uni-bremen.de>
2026-05-10 19:57:22 +0200

    initial steps for the rest api controller implementation

---
+++ src/main/java/de/matin/matinsmediasharingapp/controller/MediaItemController.java
@@ -0,0 +1,11 @@
+package de.matin.matinsmediasharingapp.controller;
+
+//TODO: implement the Controller Class.
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path ="/api/v1/media")
+public class MediaItemController {
+}
